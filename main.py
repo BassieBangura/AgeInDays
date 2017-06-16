3bb8fbcc0461d4e89126edfa4cc333e2c643fbc4
Bassie Bangura <devbassiebangura@gmail.com>
2017-06-16 18:31:30 -0400

    Coded all necessary functions and achieved a pass for all tests

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
+def isLeapYear(year):
+    '''Got this algorithm from wikipedia:
+    if (year is not divisible by 4) then (it is a common year)
+    else if (year is not divisible by 100) then (it is a leap year)
+    else if (year is not divisible by 400) then (it is a common year)
+    else (it is a leap year)'''
+    if year % 4 != 0:
+        return False
+    if year % 100 != 0:
+        return True
+    if year % 400 != 0:
+        return False
+    else:
+        return True
+
+
+def daysInMonth(year, month):
+    '''This returns the number of days in a month
+    and that value is used in the nextDay function'''
+    if month in (1, 3, 5, 7, 8, 10, 12):
+        return 31
+    else:
+        if month == 2:
+            if isLeapYear(year):
+                return 29
+            else:
+                return 28
+        else:
+            return 30
+
+def nextDay(year, month, day):
+    """Simple version: assume every month has 30 days"""
+    if day < daysInMonth(year, month):
+        return year, month, day + 1
+    else:
+        if month == 12:
+            return year + 1, 1, 1
+        else:
+            return year, month + 1, 1
 
 def dateIsBefore (year1, month1, day1, year2, month2, day2):
     '''This function dateIsBefore checks if year1-month1-day1
@@ -11,4 +50,37 @@ def dateIsBefore (year1, month1, day1, year2, month2, day2):
         if month1 == month2:
             return day1 < day2
     return False
-print dateIsBefore(1995,4,3,1999,4,1) #test for dateIsBefore() function
+#print dateIsBefore(1995,4,3,1999,4,1) #test for dateIsBefore() function
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar. Also assumes all months have
+       30 days"""
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        days += 1
+    return days
+
+def test():
+    test_cases = [((2012,1,1,2012,2,28), 58),
+                  ((2012,1,1,2012,3,1), 60),
+                  ((2011,6,30,2012,6,30), 366),
+                  ((2011,1,1,2012,8,8), 585 ),
+                  ((1900,1,1,1999,12,31), 36523)]
+
+    for (args, answer) in test_cases:
+        result = daysBetweenDates(*args)
+        if result != answer:
+            print "Test with data:", args, "failed"
+        else:
+            print "Test case passed!"
+
+test()
+print isLeapYear(2012)
+print isLeapYear(2014)
+print isLeapYear(2020)
+print isLeapYear(2010)
+print isLeapYear(2013)
